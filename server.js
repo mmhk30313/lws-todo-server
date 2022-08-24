fa819dc54a499f6ce598f0689fc44fffcf5bf78f
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2022-08-24 22:10:28 +0600

    Updated with 2 files server.js and PROCFILE

---
+++ server.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db_todos.json');
+
+const middleWares = jsonServer.defaults({
+  static: './build',
+});
+
+const PORT = process.env.PORT || 9000;
+server.use(middleWares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+
+
+//PROCFILE
+// web: node server.js
